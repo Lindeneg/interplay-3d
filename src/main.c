1bc0e9a55c43146331897bc938c8753c0b139a81
CL <cl@CLs-MacBook-Pro.local>
2024-06-02 12:36:27 +0200

    make compile, link and run on mac work
    could've been worse

--- src/main.c
+++ src/main.c
@@ -9,7 +9,7 @@
 int main(int argc, char *argv[]) {
     char config_file[PATH_LENGTH] = "./config.interplay";
     if (argc > 1 && strncmp(argv[1], "config=", 1) == 0) {
-        sscanf_s(argv[1], "config=%s", config_file, PATH_LENGTH);
+        sscanf(argv[1], "config=%s", config_file);
     }
     // parse config
     if (!context_parse_config_file(config_file)) {
